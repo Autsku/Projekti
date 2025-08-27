9cc2d0332aa3dc3619031a2bbdcaa1a6ec6e85ed
Aurora Aaltonen <Aurora.Aaltonen@kukkula.local>
2025-08-27 14:12:36 +0300

    update

---
+++ Projekti/SQL/team1.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 27, 2025 at 01:10 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `team1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurssikirjautuminen`
+--
+
+CREATE TABLE `kurssikirjautuminen` (
+  `Tunnus` int(11) NOT NULL,
+  `Opiskelija` int(11) NOT NULL,
+  `Kurssi` int(11) NOT NULL,
+  `Kirjautumispäivä` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurssit`
+--
+
+CREATE TABLE `kurssit` (
+  `Tunnus` int(11) NOT NULL,
+  `Nimi` varchar(30) NOT NULL,
+  `Kuvaus` text NOT NULL,
+  `Alkupäivä` datetime NOT NULL,
+  `Loppupäivä` datetime NOT NULL,
+  `Opettaja` varchar(30) NOT NULL,
+  `Tila` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `opettajat`
+--
+
+CREATE TABLE `opettajat` (
+  `Tunnusnumero` int(11) NOT NULL,
+  `Etunimi` varchar(30) NOT NULL,
+  `Sukunimi` varchar(30) NOT NULL,
+  `Aine` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `opiskelijat`
+--
+
+CREATE TABLE `opiskelijat` (
+  `Opiskelijanumero` int(11) NOT NULL,
+  `Etunimi` varchar(20) NOT NULL,
+  `Sukunimi` varchar(20) NOT NULL,
+  `Syntymäpäivä` date NOT NULL,
+  `Vuosikurssi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tilat`
+--
+
+CREATE TABLE `tilat` (
+  `Tunnus` int(11) NOT NULL,
+  `Nimi` varchar(30) NOT NULL,
+  `Kapasiteetti` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kurssikirjautuminen`
+--
+ALTER TABLE `kurssikirjautuminen`
+  ADD PRIMARY KEY (`Tunnus`);
+
+--
+-- Indexes for table `kurssit`
+--
+ALTER TABLE `kurssit`
+  ADD PRIMARY KEY (`Tunnus`);
+
+--
+-- Indexes for table `opettajat`
+--
+ALTER TABLE `opettajat`
+  ADD PRIMARY KEY (`Tunnusnumero`);
+
+--
+-- Indexes for table `opiskelijat`
+--
+ALTER TABLE `opiskelijat`
+  ADD PRIMARY KEY (`Opiskelijanumero`);
+
+--
+-- Indexes for table `tilat`
+--
+ALTER TABLE `tilat`
+  ADD PRIMARY KEY (`Tunnus`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kurssikirjautuminen`
+--
+ALTER TABLE `kurssikirjautuminen`
+  MODIFY `Tunnus` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kurssit`
+--
+ALTER TABLE `kurssit`
+  MODIFY `Tunnus` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `opettajat`
+--
+ALTER TABLE `opettajat`
+  MODIFY `Tunnusnumero` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `opiskelijat`
+--
+ALTER TABLE `opiskelijat`
+  MODIFY `Opiskelijanumero` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tilat`
+--
+ALTER TABLE `tilat`
+  MODIFY `Tunnus` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
