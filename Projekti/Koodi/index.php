956ac723fc1337998bcabd38a744686314ec8ac2
Aurora Aaltonen <Aurora.Aaltonen@kukkula.local>
2025-09-03 12:13:18 +0300

    update

---
+++ Projekti/Koodi/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logo</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Logo</h1>
+
+        <h3>Tiedot</h3>
+        <h3>Tilat</h3>
+        <h3>Kurssit</h3>
+        <h3>Opiskelijat</h3>
+        <h3>Opettajat</h3>
+    </div>
+</body>
+</html>
