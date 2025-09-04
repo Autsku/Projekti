3c3de2640f253d8d3449ce13c29d565c4dfde974
Aurora Aaltonen <Aurora.Aaltonen@kukkula.local>
2025-09-04 09:17:21 +0300

    update code

--- Projekti/Koodi/tiedot.php
+++ Projekti/Koodi/tiedot.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logo</title>
-</head>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"></head>
 <body>
     <div class="header">
         <a href="index.php" class="logo">Logo</a>
@@ -17,5 +18,130 @@
         </div>
     </div>
 
+    <div class="box" style="margin: 50px;">
+        <div class="oppilaat">
+            <h3>Oppilaat</h3>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Etunimi</th>
+                        <th>Sukunimi</th>
+                        <th>Syntymäpäivä</th>
+                        <th>Vuosikurssi</th>
+                    </tr>
+                <thead>
+                
+                <tbody>
+                    <tr>
+                        <td>ID</td>
+                        <td>Etunimi</td>
+                        <td>Sukunimi</td>
+                        <td>Syntymäpäivä</td>
+                        <td>Vuosikurssi</td>
+
+                        <td>
+                            <a href='Update'>Update</a>
+                            <a href='Delete'>Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="opettajat">
+            <h3>Opettajat</h3>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Etunimi</th>
+                        <th>Sukunimi</th>
+                        <th>Syntymäpäivä</th>
+                        <th>Vuosikurssi</th>
+                    </tr>
+                <thead>
+                
+                <tbody>
+                    <tr>
+                        <td>ID</td>
+                        <td>Etunimi</td>
+                        <td>Sukunimi</td>
+                        <td>Syntymäpäivä</td>
+                        <td>Vuosikurssi</td>
+
+                        <td>
+                            <a href='Update'>Update</a>
+                            <a href='Delete'>Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="kurssit">
+            <h3>Kurssit</h3>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Etunimi</th>
+                        <th>Sukunimi</th>
+                        <th>Syntymäpäivä</th>
+                        <th>Vuosikurssi</th>
+                    </tr>
+                <thead>
+                
+                <tbody>
+                    <tr>
+                        <td>ID</td>
+                        <td>Etunimi</td>
+                        <td>Sukunimi</td>
+                        <td>Syntymäpäivä</td>
+                        <td>Vuosikurssi</td>
+
+                        <td>
+                            <a href='Update'>Update</a>
+                            <a href='Delete'>Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="kirjautumiset">
+            <h3>Kirjautumiset</h3>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Etunimi</th>
+                        <th>Sukunimi</th>
+                        <th>Syntymäpäivä</th>
+                        <th>Vuosikurssi</th>
+                    </tr>
+                <thead>
+                
+                <tbody>
+                    <tr>
+                        <td>ID</td>
+                        <td>Etunimi</td>
+                        <td>Sukunimi</td>
+                        <td>Syntymäpäivä</td>
+                        <td>Vuosikurssi</td>
+
+                        <td>
+                            <a href='Update'>Update</a>
+                            <a href='Delete'>Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
